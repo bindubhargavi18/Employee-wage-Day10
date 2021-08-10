42915455b9831cb28226fade1b4bb5b672f06eeb
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-08-11 00:02:13 +0530

    Added:calculated part time employee

--- EmployeeWageMain.java
+++ EmployeeWageMain.java
@@ -1,6 +1,7 @@
 class EmployeeWageMain
 {
 	final static int fullTime=1;
+	final static int partTime=2;
 	static int empWageHr=20;
 	static int empHrs=0;
 	static int empWage=0;
@@ -11,13 +12,20 @@ class EmployeeWageMain
 	}
 	static void empWageCal()
 	{
-		int emp=(int) Math.floor(Math.random()*10)%2;
+		int emp=(int) Math.floor(Math.random()*10)%3;
 		if(emp==1)
 		{
 			empHrs=8;
-			System.out.println("Employee is present");
+			System.out.println("Employee is present andf full time");
 			empWage=empHrs*empWageHr;
-			System.out.println("Salary for employee: "+empWage);
+			System.out.println("Salary for full time employee: "+empWage);
+		}
+		else if(emp==2)
+		{
+			empHrs=4;
+			System.out.println("Employee is present and is part time");
+			empWage=empHrs*empWageHr;
+			System.out.println("Salary for partime employee: "+empWage);
 		}
 		else
 		{
