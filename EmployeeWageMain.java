1b238efe9000eb60f67bc2b6a3d354cebc9698a3
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-08-10 21:32:52 +0530

    Added:Employee is present or absent

--- EmployeeWageMain.java
+++ EmployeeWageMain.java
@@ -3,5 +3,11 @@ class EmployeeWageMain
 	public static void main(String args[])
 	{
 		System.out.println("Welcome to employee wage computation Program");
+		int fullTime=1;
+		int emp=(int) Math.floor(Math.random()*10)%2;
+		if(emp==1)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
 	}
 }
