84e91d29461b7d64fa4f6d87197e357306d0965d
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-08-10 23:57:32 +0530

    Added:calculated employee wage

--- EmployeeWageMain.java
+++ EmployeeWageMain.java
@@ -1,13 +1,30 @@
 class EmployeeWageMain
 {
+	final static int fullTime=1;
+	static int empWageHr=20;
+	static int empHrs=0;
+	static int empWage=0;
 	public static void main(String args[])
 	{
 		System.out.println("Welcome to employee wage computation Program");
-		int fullTime=1;
+		empWageCal();
+	}
+	static void empWageCal()
+	{
 		int emp=(int) Math.floor(Math.random()*10)%2;
 		if(emp==1)
+		{
+			empHrs=8;
 			System.out.println("Employee is present");
+			empWage=empHrs*empWageHr;
+			System.out.println("Salary for employee: "+empWage);
+		}
 		else
+		{
+			empHrs=0;
 			System.out.println("Employee is absent");
+			empWage=empHrs*empWageHr;
+			System.out.println("Employee Salary is: "+empWage);
+		}
 	}
 }
