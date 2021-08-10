2dda3142e949a7c570c0a521e260ac085e64886d
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-08-11 00:09:32 +0530

    Added:calculated using switch case

--- EmployeeWageMain.java
+++ EmployeeWageMain.java
@@ -13,26 +13,24 @@ class EmployeeWageMain
 	static void empWageCal()
 	{
 		int emp=(int) Math.floor(Math.random()*10)%3;
-		if(emp==1)
+		switch(emp)
 		{
-			empHrs=8;
-			System.out.println("Employee is present andf full time");
-			empWage=empHrs*empWageHr;
-			System.out.println("Salary for full time employee: "+empWage);
-		}
-		else if(emp==2)
-		{
-			empHrs=4;
-			System.out.println("Employee is present and is part time");
-			empWage=empHrs*empWageHr;
-			System.out.println("Salary for partime employee: "+empWage);
-		}
-		else
-		{
-			empHrs=0;
-			System.out.println("Employee is absent");
-			empWage=empHrs*empWageHr;
-			System.out.println("Employee Salary is: "+empWage);
+		   case fullTime:empHrs=8;
+				         System.out.println("Employee is present and is full time");
+			             empWage=empHrs*empWageHr;
+			             System.out.println("Salary for full time employee: "+empWage);
+			             break;
+		
+		   case partTime:empHrs=4;
+			             System.out.println("Employee is present and is part time");
+			             empWage=empHrs*empWageHr;
+			             System.out.println("Salary for partime employee: "+empWage);
+			             break;
+
+		   default:empHrs=0;
+			       System.out.println("Employee is absent");
+			       empWage=empHrs*empWageHr;
+			       System.out.println("Employee Salary is: "+empWage);
 		}
 	}
 }
