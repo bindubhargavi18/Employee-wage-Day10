2a18f116d0b4c0ced21ba0d3ff3184dba8e9c5ec
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-08-11 00:18:57 +0530

    Added:Calculated wages for month

--- EmployeeWageMain.java
+++ EmployeeWageMain.java
@@ -3,8 +3,7 @@ class EmployeeWageMain
 	final static int fullTime=1;
 	final static int partTime=2;
 	static int empWageHr=20;
-	static int empHrs=0;
-	static int empWage=0;
+	static int workingDays=20;
 	public static void main(String args[])
 	{
 		System.out.println("Welcome to employee wage computation Program");
@@ -12,25 +11,31 @@ class EmployeeWageMain
 	}
 	static void empWageCal()
 	{
+		int empHrs=0;
+	    int empWage=0;
+	    int totalWage=0;
 		int emp=(int) Math.floor(Math.random()*10)%3;
 		switch(emp)
 		{
 		   case fullTime:empHrs=8;
 				         System.out.println("Employee is present and is full time");
 			             empWage=empHrs*empWageHr;
-			             System.out.println("Salary for full time employee: "+empWage);
+			             totalWage=empWage*workingDays;
+			             System.out.println("Salary for a month: "+totalWage);
 			             break;
 		
 		   case partTime:empHrs=4;
 			             System.out.println("Employee is present and is part time");
 			             empWage=empHrs*empWageHr;
-			             System.out.println("Salary for partime employee: "+empWage);
+			             totalWage=empWage*workingDays;
+			             System.out.println("Salary for a month: "+totalWage);
 			             break;
 
 		   default:empHrs=0;
 			       System.out.println("Employee is absent");
 			       empWage=empHrs*empWageHr;
-			       System.out.println("Employee Salary is: "+empWage);
+			       totalWage=empWage*workingDays;
+			       System.out.println("Employee Salary is: "+totalWage);
 		}
 	}
 }
